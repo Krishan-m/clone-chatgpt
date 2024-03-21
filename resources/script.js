8ce7cd5792fbe6bee1a97d69ff000ff36dda48d8
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-03-21 15:25:17 -0400

    Add layout and styling

--- resources/script.js
+++ resources/script.js
@@ -1,26 +1,63 @@
 const sidebar = document.querySelector('aside');
-        const sidebarCloser = document.getElementById('closerContainer');
-        const sidebarCloserTop = sidebarCloser.children[0];
-        const sidebarCloserBottom = sidebarCloser.children[1];
+const main = document.querySelector('main');
+const sidebarCloser = document.getElementById('closerContainer');
+const sendChat = document.getElementById('sendChat');
+const userQuery = document.getElementById("userQuery");
+const sidebarCloserTop = sidebarCloser.children[0];
+const sidebarCloserBottom = sidebarCloser.children[1];
+const username = "John Wick";
 
-        sidebarCloser.addEventListener('mouseenter', () => {
-            if (sidebar.classList.contains('hide')) {
-                sidebarCloserTop.classList.add('anti-rotate')
-                sidebarCloserBottom.classList.add('rotate')
-            }
-            else {
-                sidebarCloserTop.classList.add('rotate')
-                sidebarCloserBottom.classList.add('anti-rotate')
-            }
-        })
+// Adding user query to chatbox
+function addResponseToChatBox(message, sender) {
+    if (sender.toLowerCase() == "user") sender = "You";
+    else if (sender.toLowerCase() == "bot") sender = "ChatGPTT";
+    else throw Error("Invalid value for parameter 'sender'");
+    let messageBox = document.createElement('div');
+    messageBox.classList.add('user-message'); // Add different class for 
+    messageBox.innerHTML = `<span><b>${sender}</b></span><p>${message}</p>`;
+    document.getElementById('chatDisplay').insertAdjacentElement('beforeend', messageBox);
+}
 
-        sidebar.addEventListener('mouseleave', (e) => {
-            sidebarCloserTop.classList.remove('anti-rotate')
-            sidebarCloserTop.classList.remove('rotate')
-            sidebarCloserBottom.classList.remove('rotate')
-            sidebarCloserBottom.classList.remove('anti-rotate')
-        })
+function postUserQuery() {
+    let query = userQuery.value;
+    addResponseToChatBox(message = query, sender = "user");
+    userQuery.value = "";
+    getBotResponse();
+}
 
-        sidebarCloser.addEventListener('click', () => {
-            sidebar.classList.toggle('hide')
-        })
+function getBotResponse() {
+    setTimeout(() => {
+        let botResponse = "This is what GPT said..."; // Add orignal response from GPT
+    }, 1000);
+    addResponseToChatBox(botResponse, sender = "bot");
+}
+
+// Event Listeners
+sidebarCloser.addEventListener('mouseenter', () => {
+    if (!sidebar.classList.contains('hide')){
+        sidebarCloserTop.classList.add('rotate')
+        sidebarCloserBottom.classList.add('anti-rotate')
+    }
+})
+
+sidebar.addEventListener('mouseleave', () => {
+    sidebarCloserTop.classList.remove('anti-rotate')
+    sidebarCloserTop.classList.remove('rotate')
+    sidebarCloserBottom.classList.remove('rotate')
+    sidebarCloserBottom.classList.remove('anti-rotate')
+})
+
+sidebarCloser.addEventListener('click', () => {
+    sidebar.classList.toggle('hide')
+    main.classList.toggle('main-fullwidth')
+    if (sidebar.classList.contains('hide')) {
+        sidebarCloserTop.classList.add('anti-rotate')
+        sidebarCloserBottom.classList.add('rotate')
+    }
+})
+
+sendChat.addEventListener('click', postUserQuery)
+
+userQuery.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') postUserQuery();
+})
